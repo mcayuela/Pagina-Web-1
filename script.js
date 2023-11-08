48df8c16fda75fd9fe65db40b921dd247a54afd8
mcayuela <149514264+mcayuela@users.noreply.github.com>
2023-11-08 17:50:17 +0000

    Deploying to gh-pages from @ mcayuela/Pagina-Web-1@df115c57ee9809e0c045febdd4708a8faa0eac1f 🚀

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtenemos una referencia a los elementos del menú de navegación
+    // Obtenim una referència als elements del menu de navegació
     var homeLink = document.querySelector("nav ul li:nth-child(1) a");
     var aboutUsLink = document.querySelector("nav ul li:nth-child(2) a");
     var servicesLink = document.querySelector("nav ul li:nth-child(3) a");
     var contactLink = document.querySelector("nav ul li:nth-child(4) a");
 
-    // Funciones que se ejecutarán al hacer clic en los botones
+    // Funcions que se ejecutarán al hacer clic en los botones
     function goToHome() {
-        // Aquí puedes agregar el código para redirigir a la página de inicio
+        // Aqui pots agregar el codi per a dirigir a la pàgina d'inici
         alert("Anar a la pàgina d'inici");
     }
 
     function goToAboutUs() {
-        // Aquí puedes agregar el código para redirigir a la página "Sobre Nosaltres"
+        // Aqui pots agregar el codi per a dirigir a la pàgina "Sobre Nosaltres"
         alert("Anar a la pàgina 'Sobre Nosaltres'");
     }
 
     function goToServices() {
-        // Aquí puedes agregar el código para redirigir a la página de servicios
+        // Aqui pots agregar el codi per a dirigir a la pàgina de serveis
         alert("Anar a la pàgina de serveis");
     }
 
     function goToContact() {
-        // Aquí puedes agregar el código para redirigir a la página de contacto
+        // Aqui pots agregar el codi per a dirigir a la pàgina de contacte
         alert("Anar a la pàgina de contacte");
     }
 
-    // Agregar manejadores de eventos a los botones
+    // Agregar manejadors d'events als botons
     homeLink.addEventListener("click", goToHome);
     aboutUsLink.addEventListener("click", goToAboutUs);
     servicesLink.addEventListener("click", goToServices);
